b4318696b9b9fc26a4e3dbb5dc7e267a601e6788
MichaelRafael <michael-ras@hotmail.com>
2023-11-11 12:28:14 -0300

    Criação das classes model

---
+++ bibliotecaSab/src/main/java/com/fuctura/bibliotecaSab/model/Categoria.java
@@ -0,0 +1,56 @@
+package com.fuctura.bibliotecaSab.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categoria {
+
+    private int id;
+    private String nome;
+    private String descricao;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria() {
+
+    }
+
+    public Categoria(int id, String nome, String descricao, List<Livro> livros) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.livros = livros;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+}
---
+++ bibliotecaSab/src/main/java/com/fuctura/bibliotecaSab/model/Livro.java
@@ -0,0 +1,62 @@
+package com.fuctura.bibliotecaSab.model;
+
+public class Livro {
+
+    private int id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+
+    private Categoria categoria;
+
+    public Livro() {
+    }
+
+    public Livro(int id, String titulo, String nome_autor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nome_autor = nome_autor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNome_autor() {
+        return nome_autor;
+    }
+
+    public void setNome_autor(String nome_autor) {
+        this.nome_autor = nome_autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
