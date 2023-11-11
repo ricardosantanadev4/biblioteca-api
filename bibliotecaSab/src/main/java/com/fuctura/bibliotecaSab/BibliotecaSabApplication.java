9d11fa5ffb70b5e8735a907cfe71cb23a4e4e877
MichaelRafael <michael-ras@hotmail.com>
2023-11-11 11:22:38 -0300

    primeiro commit

---
+++ bibliotecaSab/src/main/java/com/fuctura/bibliotecaSab/BibliotecaSabApplication.java
@@ -0,0 +1,13 @@
+package com.fuctura.bibliotecaSab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaSabApplication.class, args);
+	}
+
+}
---
+++ bibliotecaSab/src/test/java/com/fuctura/bibliotecaSab/BibliotecaSabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuctura.bibliotecaSab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
